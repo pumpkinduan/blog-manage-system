8cb6e2471db43235244733d3ab8f75f14f5e4fe9
pumpkin <pumpkin_1208@163.com>
2020-10-04 20:52:53 +0800

    Markdown编辑器增加错误校验样式

--- src/components/ArticleCreate/MarkdownEditor/index.tsx
+++ src/components/ArticleCreate/MarkdownEditor/index.tsx
@@ -1,15 +1,19 @@
-import React, { Component } from 'react';
-import SimpleMDE from 'simplemde';
-import '../../../assets/styles/simplemde.scss';
-import './index.scss';
-import { Sitdown } from 'sitdown';
+import React, { Component } from "react";
+import SimpleMDE from "simplemde";
+import "../../../assets/styles/simplemde.scss";
+import "./index.scss";
+import { Sitdown } from "sitdown";
+import _ from "loadsh";
 interface IProps {
-	content: string;
+	content?: string;
 }
 let sitdown = new Sitdown();
 class MarkdownEditor extends Component<IProps> {
 	editor: SimpleMDE;
 	markedEditorRef: React.RefObject<HTMLTextAreaElement> = React.createRef();
+	state = {
+		isEmpty: false,
+	};
 	componentDidMount() {
 		let config = {
 			renderingConfig: {
@@ -29,11 +33,11 @@ class MarkdownEditor extends Component<IProps> {
 				delay: 3000,
 
 				//在storage中 会自动生成key为smde_marked_text的键代表markdown格式的内容
-				uniqueId: 'marked_text',
+				uniqueId: "marked_text",
 			},
 			promptURLs: true,
 
-			placeholder: '开始你的写作之旅吧~',
+			placeholder: "写点东西吧~",
 
 			tabSize: 4,
 		};
@@ -42,7 +46,20 @@ class MarkdownEditor extends Component<IProps> {
 			element: this.markedEditorRef.current,
 			...config,
 		}));
-
+		editor.codemirror.on(
+			"change",
+			_.debounce(() => {
+				if (!editor.value()) {
+					this.setState({
+						isEmpty: true,
+					});
+				} else {
+					this.setState({
+						isEmpty: false,
+					});
+				}
+			}, 300)
+		);
 		// 1. 由编辑文章进入，显示已经发布了，并获取到的数据(数据为markdown格式的，并非HTML格式)
 		// 2. 由创建文章进入，显示空数据或是编辑过但没有发布的数据(如：路由切换，数据保存至storage)
 		// 由于该编辑器自动实现了，所以第2步不用自己实现（参见：autosave的配置）
@@ -50,10 +67,18 @@ class MarkdownEditor extends Component<IProps> {
 		let markdownText =
 			this.props.content && sitdown.HTMLToMD(this.props.content);
 		markdownText && editor.value(markdownText);
+		this.setState({ isEmpty: !!markdownText });
 	}
 	render() {
+		const { isEmpty } = this.state;
 		return (
-			<div className="markdownEditorApp-container">
+			<div
+				className={
+					isEmpty
+						? "markdownEditorApp-container markdown-error"
+						: "markdownEditorApp-container"
+				}
+			>
 				<textarea id="markdownEditorApp" ref={this.markedEditorRef} />
 			</div>
 		);
