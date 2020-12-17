2817f4d1f27d1fd4e8290cfb9a8eee185273e8a6
pumpkinduan <34852848+pumpkinduan@users.noreply.github.com>
2020-12-17 11:03:53 +0800

    封装的基础formItem：去除多余类型字段

--- src/common/BasicFormItem/BasicFormItem.tsx
+++ src/common/BasicFormItem/BasicFormItem.tsx
@@ -1,76 +1,56 @@
-import React from "react";
-import { Form, Input } from "antd";
-import { FormItemProps } from "antd/lib/form";
-import { validatorResultProps } from "utils/validators";
-import "./index.scss";
-const { Password } = Input;
-
+import React from 'react';
+import { Form, Input } from 'antd';
+import { FormItemProps } from 'antd/lib/form';
+import { validatorResultProps } from 'utils/validators';
+import './index.scss';
 export interface BasicFormItemProps extends FormItemProps {
-	// 自定义的规则校验器，必须遵循tools文件中validatePwd这类函数返回的格式
 	customValidator?: (value: string) => validatorResultProps;
-	type?: string; // 输入框类型
-	disabled?: boolean; // 是否禁用输入框
-	maxLength?: number;
 	nonErrMessage?: string; // 输入框为空时，校验失败的报错信息
-	size?: any;
 	customController?: React.ReactNode;
 }
 class BasicFormItem extends React.PureComponent<BasicFormItemProps> {
 	render() {
 		const {
-			hasFeedback = true,
-			nonErrMessage = "该项必填",
-			name,
-			type = "text",
-			initialValue,
-			disabled,
-			maxLength = 63,
-			required = true,
+			nonErrMessage = '该项必填',
 			customValidator,
 			customController,
 			...rest
 		} = this.props;
-		const MInput = type === "password" ? Password : Input;
+
 		return (
 			<Form.Item
+				required={true}
+				hasFeedback={true}
 				className="override-ant-form-item"
-				initialValue={initialValue}
-				hasFeedback={hasFeedback}
-				name={name}
 				{...rest}
 				rules={[
-					{ required: required, message: nonErrMessage },
+					{ required: true, message: nonErrMessage },
 					({ getFieldValue }) => ({
 						validator(rule, value) {
 							// 若是密码确认框，则校验逻辑不一样
-							if (name === "confirm") {
+							if (rest.name === 'confirm') {
 								if (
 									!value ||
-									getFieldValue("password") === value
+									getFieldValue('password') === value
 								) {
 									return Promise.resolve();
 								}
-								return Promise.reject("两次输入的密码不一致");
+								return Promise.reject('两次输入的密码不一致');
 							}
-							if (!required || !value) {
+							if (!value) {
 								return Promise.resolve();
 							}
 							let res = customValidator
 								? customValidator(value)
-								: { msg: "", status: true };
+								: { msg: '', status: true };
 							if (res.status) {
 								return Promise.resolve();
 							}
 							return Promise.reject(res.msg);
 						},
 					}),
-				]}
-			>
-				{customController ? (
-					customController
-				) : (
-					<MInput maxLength={maxLength} disabled={disabled} />
-				)}
+				]}>
+				{customController ? customController : <Input />}
 			</Form.Item>
 		);
 	}
