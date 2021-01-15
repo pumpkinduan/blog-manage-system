40dcf3be2932975c67d59a946fd3293e8192d74d
pumpkinduan <34852848+pumpkinduan@users.noreply.github.com>
2021-01-15 10:29:56 +0800

    图片类型增加头像

--- src/interfaces/photo.interface.ts
+++ src/interfaces/photo.interface.ts
@@ -1,11 +1,11 @@
 export enum PHOTO_TYPE {
-    POST = 'POST', // 文章的图片
-    WALL = 'WALL'// 相册墙
+    POST = 'POST',      // 文章的图片
+    WALL = 'WALL',    // 相册墙
+    AVATAR = 'AVATAR' //头像
 }
 export interface CreatePhoto {
     type: PHOTO_TYPE;
 }
-
 export interface BasicPhoto {
     id: string;
     path: string;
